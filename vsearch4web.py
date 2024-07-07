50cb2197874baf0ba0dc7d13424a6eda3bd5bef4
Breedlove-Jason <j@jasonbreedlove.com>
2024-07-06 17:40:48 -0700

    turn log results output into table

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from markupsafe import escape
 from app import search_for_letters
 
 app = Flask(__name__)
@@ -13,6 +13,11 @@ def entry_page() -> "html":
     return render_template(
         "entry.html", the_title="Welcome to search4letters on the web!"
     )
+
+
+def log_request(req: "flask_request", res: str) -> None:
+    with open("vsearch.log", "a") as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep="|")
 
 
 # Route to handle the search form submission
@@ -29,6 +34,7 @@ def do_search() -> str:
 
     # Perform the search
     search_results = search_for_letters(phrase, letters)
+    log_request(request, search_results)
 
     # Render the results template with the search results
     return render_template(
@@ -40,6 +46,18 @@ def do_search() -> str:
     )
 
 
+@app.route("/viewlog")
+def viewlog() -> 'html':
+    contents = []
+    with open("vsearch.log") as log:
+        for line in log:
+            contents.append([])
+            for item in line.split("|"):
+                contents[-1].append(escape(item))
+    titles = ("Form Data", "Remote_addr", "User_agent", "Results")
+    return render_template("viewlog.html", the_title="View Log", the_row_titles=titles, the_data=contents)
+
+
 # Run the Flask application with debug mode enabled on port 5005
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
